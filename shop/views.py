37847c45b53d936bf3d645e45e9115719b2ae74c
Bogomila <bkatsarska@abv.bg>
2024-03-29 08:46:34 +0200

    Initial commit

---
+++ shop/views.py
@@ -0,0 +1,28 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from shop.models import Product
+
+
+def index(request):
+    product_objects = Product.objects.all()
+
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(title__icontains=item_name)
+
+    paginator = Paginator(product_objects, 4)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+
+    context = {
+        'product_objects': product_objects,
+    }
+    return render(request, 'shop/index.html', context)
+
+
+def detail(request, id):
+    product = Product.objects.get(id=id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'shop/detail.html', context)

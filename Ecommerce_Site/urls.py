a053e6b83a23a2a0a4c5b2dc2080e45781348f5a
Bogomila <bkatsarska@abv.bg>
2024-03-30 16:19:51 +0200

    updated views

--- Ecommerce_Site/urls.py
+++ Ecommerce_Site/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from shop.views import index
+from shop.views import index, detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('<int:id>/', detail, name='detail'),
 ]
